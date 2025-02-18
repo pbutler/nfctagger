89ffaf7e309e0648aab9776ee9fae6bf5b65ff33
Patrick Butler <pbutler@killertux.org>
2025-02-18 09:15:43 -0500

    docs: added API reference for ntag docs

--- src/nfctagger/devices/ntag.py
+++ src/nfctagger/devices/ntag.py
@@ -1,3 +1,7 @@
+"""
+Reference to the Protocol: https://www.nxp.com/docs/en/data-sheet/NTAG213_215_216.pdf
+"""
+
 from typing import Union
 
 import construct as c
@@ -8,8 +12,10 @@ from . import Tag
 from ..data import Command
 from ..data import Response
 
+
 class NTagResponse(Response):
     """Generic NTAG21x response"""
+
     pass
 
 
@@ -18,6 +24,7 @@ class NTagVersionCmd(Command):
     This command is used to get the version of the ntag chip (size is
     inferrable from this), Command is a simple 0x60 byte
     """
+
     def __init__(self, *, data=None, bdata=None):
         super().__init__(data={}, bdata=bdata)
 
@@ -29,6 +36,7 @@ class NTagVersionCmd(Command):
 
 class NTagReadCmd(Command):
     """Read Command"""
+
     def _struct(self):
         return c.Struct(
             "cmd" / c.Const(b"\x30"),
@@ -38,12 +46,14 @@ class NTagReadCmd(Command):
 
 class NTagReadResp(Response):
     """Read response, contains the data read"""
+
     def _struct(self) -> c.Construct:
         return c.Struct("data" / c.GreedyBytes)  # pyright: ignore
 
 
 class NTagWriteCmd(Command):
     """Write command contains the address and datat to write"""
+
     def _struct(self):
         return c.Struct(
             "cmd" / c.Const(b"\xa2"),
@@ -53,16 +63,18 @@ class NTagWriteCmd(Command):
 
 
 class NTagWriteResp(Response):
-    """ Write response is simply an ack packet which we don't get to see"""
+    """Write response is simply an ack packet which we don't get to see"""
+
     def _struct(self) -> c.Construct:
         return c.Struct("ack" / c.GreedyBytes)  # pyright: ignore
 
 
 class NTagVersionResp(Response):
     """
-    Version response, contains identifying info which can figure out 
+    Version response, contains identifying info which can figure out
     size
     """
+
     def _struct(self):
         return c.Struct(
             "header" / c.Bytes(1),
@@ -86,6 +98,8 @@ class NTagVersionResp(Response):
 
 
 class NTag(Tag):
+    """Implementation of the NTAG21x Tag"""
+
     def __init__(self, connection: Device):
         super().__init__(connection)
         # Default values for NTAG215
@@ -99,7 +113,7 @@ class NTag(Tag):
         # TODO Implement actual identification
         return True
 
-    def write(self, cmd: Command, tunnel: bool=False) -> Response:
+    def write(self, cmd: Command, tunnel: bool = False) -> Response:
         """
         Write a command to the device
 
@@ -108,7 +122,7 @@ class NTag(Tag):
         :return: Response from the device
         """
 
-        #nothing to tunnel past here to
+        # nothing to tunnel past here to
         assert not tunnel
         resp = self._connection.write(cmd, tunnel=True)
         return NTagResponse(bdata=resp.child())
@@ -155,7 +169,6 @@ class NTag(Tag):
         assert len(data) == 4
         response = self.write(NTagWriteCmd(data={"addr": address, "data": data}))
         response = NTagWriteResp(bdata=response.bytes())
-        logger.debug(f"<{len(response)}")
 
     def mem_write_user(self, data: Union[bytes, bytearray]):
         """
@@ -163,7 +176,6 @@ class NTag(Tag):
 
         :param data: data to write
         """
-        #TOOD this can write anywhere
         address = self._user_start_page
         assert len(data) <= self._user_size
 
@@ -173,7 +185,7 @@ class NTag(Tag):
             self.mem_write4(address + i, data[i * 4 : i * 4 + 4])
         leftover = len(data) % 4
 
-        # if we send a non 4 byte chunk, read in the last block and 
+        # if we send a non 4 byte chunk, read in the last block and
         # append the data with the missing bytes
         if leftover:
             rewrites = self.mem_read4(address + writes)[leftover:4]
