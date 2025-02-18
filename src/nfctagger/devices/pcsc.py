84b1b16018cd743e7c5abbd9ed2f9967fd6ad985
Patrick Butler <pabutler@vt.edu>
2025-02-18 01:32:04 -0500

    docs: pcsc.py

--- src/nfctagger/devices/pcsc.py
+++ src/nfctagger/devices/pcsc.py
@@ -9,6 +9,7 @@ from ..data import Response, Command
 
 
 class PCSCResp(Response):
+    """Generic PCSC response"""
     def _struct(self):
         return Struct(
             "data_out" / GreedyBytes,  # pyright: ignore
@@ -19,21 +20,33 @@ class PCSCResp(Response):
 
 
 class PCSC(ParentDevice):
-    possible_children = (ACR122U,)
+    """ 
+    This device is really an adapter for PCSC/PySCard.  As of now it 
+    only send tunnel commands to actual devices
+    """
+
+    possible_children = [ACR122U,]
 
     def __init__(self, connection):
         super().__init__(connection)
 
     def write(self, cmd: Command, tunnel=False) -> Response:
-        # logger.debug(f"> {cmd}")
+        """
+        Send command to device, right now ignores the tunnel flag
+
+        :param cmd: command to send
+        :param tunnel: if true, send command to child device, defaults to False
+        :return: Rresponse received from device
+        """
+
         response, sw1, sw2 = self._connection.transmit(list(cmd.bytes()))
-        assert sw1 == 0x90 and sw2 == 0x00
-        # logger.debug(
-        #    f"< {' '.join(f'{byte:02X}' for byte in response)}, SW1: {sw1:02X}, SW2: {sw2:02X}"
-        # )
+        # check if response is okay 
+        assert sw1 == 0x90 and sw2 == 0x00, "Response not okay"
         return PCSCResp(bdata=bytearray(response))
 
     def get_tag(self) -> Tag:
+        """Grab the lowest level device and make sure it's a tag"""
+        #TODO: Make this recusrsive through parent class
         cur = self
         while hasattr(cur, "_child"):
             cur = cur._child
