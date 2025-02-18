7e0b267fa6d758e252191a713dfad610e9db3614
Patrick Butler <pabutler@vt.edu>
2025-02-17 23:29:46 -0500

    docs: documented data.py

--- src/nfctagger/data.py
+++ src/nfctagger/data.py
@@ -1,8 +1,6 @@
-from binascii import hexlify
+from typing import Optional, Union
+
 from construct import (
-    Struct,
-    Bytes,
-    Const,
     Container,
     GreedyBytes,
 )
@@ -10,7 +8,26 @@ from construct.core import Construct
 
 
 class Frame:
-    def __init__(self, *, data=None, bdata=None):
+    def __init__(
+        self,
+        *,
+        data: Optional[dict] = None,
+        bdata: Optional[Union[bytes, bytearray]] = None,
+    ):
+        """
+        A simple binary parsing and building class.  By default it
+        grabs all bytes until the end of stream.  This can be subclassed
+        to provide more specific parsing and building by overriding
+        the _struct function to return an appropriate Construct object.
+
+        Only one of data or bdata should be defined.
+
+        :param data: if defined a dictionary describing the data to be
+        represented in biary, defaults to None
+        :type data: dict, optional
+        :param bdata: the binary data which upon init will be parsed, defaults to None
+        :type bdata: bytes or bytearray, optional
+        """
         self._data: Container
         self._parser = self._struct()
         if data is not None:
@@ -25,52 +42,41 @@ class Frame:
             raise ValueError("one of data and bdata must be defined")
 
     def _struct(self) -> Construct:
+        """
+        Return the construct object that will be used to parse and build
+        the binary data.
+
+        :return: Construct object that can be used for parsing and building
+        """
         return GreedyBytes  # pyright: ignore
 
     def bytes(self) -> bytes:
+        """
+        return the binary representation of the data
+
+        :return: binary representation of the data
+        """
         return self._parser.build(self._data)
 
-    def __len__(self):
+    def __len__(self) -> int:
+        """
+        Length of the binary representation of the data
+
+        :return: Length of the binary representation of the data
+        """
         return len(self.bytes())
 
-    def __str__(self):
+    def __str__(self) -> str:
         return f"<{self.__class__.__name__}: {self._data}>"
 
 
 class Response(Frame):
+    """Response frame"""
+
     pass
 
 
 class Command(Frame):
+    """Command frame"""
+
     pass
-
-
-class PN53xInCommunicateThruResp(Response):
-    def _struct(self):
-        return Struct(
-            "header" / Bytes(2) * "0xD5 0x43",
-            "status" / Bytes(1),
-            "data_in" / GreedyBytes,  # pyright: ignore
-        )
-
-    def child(self):
-        return self._data.data_in
-
-
-class PN53xInCommunicateThruCmd(Command):
-    def _struct(self):
-        return Struct(
-            "header" / Const(b"\xd4\x42"),
-            "data_out" / GreedyBytes,  # pyright: ignore
-        )
-
-    def validate(self):
-        if len(self._data.data_out) > 264:
-            raise ValueError("data_out must be less than 264 bytes")
-
-    def child(self):
-        return self._data.data_out.build()
-
-
-if __name__ == "__main__":
-    print(PN53xInCommunicateThruCmd(data={"data_out": b"\x01\x02\x03"}))
